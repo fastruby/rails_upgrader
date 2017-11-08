98b012376fabf669d5c845be87e67d4461cadac0
Mauro Otonelli <mauro.otonelli@gmail.com>
2017-11-07 23:18:57 -0300

    Reset model as well

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,11 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
 
+def reset_dummy_files
+  reset_controller_content
+  reset_model_content
+end
+
 def reset_controller_content
   original_content = <<-END
 class UsersController < ApplicationController
@@ -19,5 +24,16 @@ end
   END
 
   path = File.join(File.dirname(__FILE__), "dummy", "app", "controllers", "users_controller.rb")
+  File.write(path, original_content)
+end
+
+def reset_model_content
+  original_content = <<-END
+class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :project_id
+end
+  END
+
+  path = File.join(File.dirname(__FILE__), "dummy", "app", "models", "user.rb")
   File.write(path, original_content)
 end
