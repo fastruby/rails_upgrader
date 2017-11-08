837f491b14627afa1edc36ec2774ed067f6789cb
Mauro Otonelli <mauro.otonelli@gmail.com>
2017-11-08 00:47:11 -0300

    Fix bug when updating controller content, renamed variables

--- lib/rails_upgrader/strong_params.rb
+++ lib/rails_upgrader/strong_params.rb
@@ -3,7 +3,7 @@ require "active_model/naming"
 module RailsUpgrader
   class StrongParams
     attr_reader :entity, :param_key, :controller_path, :model_path
-    ATTR_ACCESSIBLE_REGEX = /\s+attr_accessible\s+([:]\w+[,]?\s+)+/.freeze
+    ATTR_ACCESSIBLES = /\s+attr_accessible\s+([:]\w+[,]?\s+)+/.freeze
 
     def initialize(entity)
       @entity = entity
@@ -17,7 +17,7 @@ module RailsUpgrader
     end
 
     def update_controller_content!
-      updated_content = append_strong_params
+      updated_content = appended_strong_params
 
       File.open(controller_path, 'wb') do |file|
         file.write(updated_content)
@@ -34,9 +34,17 @@ module RailsUpgrader
 
     private
 
-      def append_strong_params
+      def appended_strong_params
+        result = controller_content
         last_end = controller_content.rindex("end")
-        controller_content[last_end..last_end+3] = "\n#{generate_method}end\n"
+        result[last_end..last_end+3] = "\n#{generate_method}end\n"
+        result
+      end
+
+      def removed_attr_accessible
+        result = model_content
+        result[ATTR_ACCESSIBLES] = "\n"
+        result
       end
 
       def controller_content
@@ -60,12 +68,6 @@ module RailsUpgrader
           result += "  # TODO: check nested attributes for: #{entity.model.nested_attributes_options.keys.join(', ')}\n"
         end
         result += "  end\n\n"
-        result
-      end
-
-      def removed_attr_accessible
-        result = model_content
-        result[/\s+attr_accessible\s+([:]\w+[,]?\s+)+/] = "\n"
         result
       end
   end
