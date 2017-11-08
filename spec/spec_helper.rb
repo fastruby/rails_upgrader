23b26a229ef1f3af59bd143e9479c23edb724111
Mauro Otonelli <mauro.otonelli@gmail.com>
2017-11-07 23:11:06 -0300

    Added helper for overwriting users_controller.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -8,4 +10,14 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+def reset_controller_content
+  original_content = <<-END
+class UsersController < ApplicationController
+end
+  END
+
+  path = File.join(File.dirname(__FILE__), "dummy", "app", "controllers", "users_controller.rb")
+  File.write(path, original_content)
 end
